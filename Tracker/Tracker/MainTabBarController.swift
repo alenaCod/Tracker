36333e550dc20718284d29cc05acdecf0260ba2b
Sveta <sveta@Svitlanas-MacBook-Pro-2.local>
2018-05-07 22:19:17 +0300

    commit

---
+++ Tracker/Tracker/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  Tracker
+//
+//  Created by Sveta on 5/2/18.
+//  Copyright © 2018 Alena. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
---
+++ Tracker/Tracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tracker
+//
+//  Created by Sveta on 4/29/18.
+//  Copyright © 2018 Alena. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
