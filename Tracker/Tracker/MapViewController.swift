98d4412f038c4d72f2bfde306eb6988b9d8ba02d
alenaCod <moisal1987@gmail.com>
2018-06-10 01:06:29 +0300

    add Map

---
+++ Tracker/Tracker/MapViewController.swift
@@ -0,0 +1,75 @@
+//
+//  MapViewController.swift
+//  Tracker
+//
+//  Created by Sveta on 6/4/18.
+//  Copyright © 2018 Alena. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+  
+  @IBOutlet weak var theMap: MKMapView!
+  
+  @IBAction func CancelButton(_ sender: UIBarButtonItem) {
+    self.dismiss(animated: true, completion: nil)
+  }
+  //  @IBOutlet weak var theLabel: UILabel!
+  
+  var manager = CLLocationManager()
+  var myLocations: [CLLocation] = []
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    //Setup our Location Manager
+    //manager = CLLocationManager()
+    manager.delegate = self
+    manager.desiredAccuracy = kCLLocationAccuracyBest
+    manager.requestWhenInUseAuthorization()
+    manager.requestAlwaysAuthorization()
+    manager.startUpdatingLocation()
+    
+    //Setup our Map View
+    theMap.delegate = self
+    theMap.mapType = MKMapType.standard
+    theMap.showsUserLocation = true
+    theMap.showsScale = true
+  }
+
+  internal func locationManager(_ manager:CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+    //var myLocation = locations[0]
+   // theLabel.text = "\(locations[0])"
+    myLocations.append(locations[0] )
+    
+    let spanX = 0.01
+    let spanY = 0.01
+    let newRegion = MKCoordinateRegion(center: theMap.userLocation.coordinate, span: MKCoordinateSpanMake(spanX, spanY))
+    theMap.setRegion(newRegion, animated: true)
+    
+    if (myLocations.count > 1){
+      let sourceIndex = myLocations.count - 1
+      let destinationIndex = myLocations.count - 2
+      
+      let c1 = myLocations[sourceIndex].coordinate
+      let c2 = myLocations[destinationIndex].coordinate
+      var a = [c1, c2]
+      let polyline = MKPolyline(coordinates: &a, count: a.count)
+      theMap.add(polyline)
+    }
+  }
+  
+  func mapView(_ mapView: MKMapView!, rendererFor overlay: MKOverlay!) -> MKOverlayRenderer! {
+    
+    if overlay is MKPolyline {
+      let polylineRenderer = MKPolylineRenderer(overlay: overlay)
+      polylineRenderer.strokeColor = UIColor.gray
+      polylineRenderer.lineWidth = 2
+      return polylineRenderer
+    }
+    return nil
+  }
+}
